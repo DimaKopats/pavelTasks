cced92c9a6519282aa902881d4f6167582103afe
Dzmitry Kopats <dzmitry_kopats@epbyminw2670.minsk.epam.com>
2017-11-16 11:41:16 +0300

    add collectionView reorder

---
+++ CollectionView_reorderCell/CollectionView_reorderCell/Assets.xcassets/ABCCell.swift
@@ -0,0 +1,14 @@
+//
+//  ABCCell.swift
+//  CollectionView_reorderCell
+//
+//  Created by Dzmitry Kopats on 11/16/17.
+//  Copyright © 2017 Dzmitry Kopats. All rights reserved.
+//
+
+import UIKit
+
+class ABCCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ABCLabel: UILabel!
+}
---
+++ CollectionView_reorderCell/CollectionView_reorderCell/CollectionViewController.swift
@@ -0,0 +1,104 @@
+//
+//  CollectionViewController.swift
+//  CollectionView_reorderCell
+//
+//  Created by Dzmitry Kopats on 11/16/17.
+//  Copyright © 2017 Dzmitry Kopats. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    var charArr = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let letters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+        
+        for letterIndex in letters.indices {
+            charArr.append(String(letters[letterIndex]))
+        }
+        
+        // Register cell classes
+//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return charArr.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ABCCell
+
+        cell.ABCLabel.text = charArr[indexPath.item]
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let temp = charArr[sourceIndexPath.item]
+        charArr[sourceIndexPath.item] = charArr[destinationIndexPath.item]
+        charArr[destinationIndexPath.item] = temp
+    }
+        
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
