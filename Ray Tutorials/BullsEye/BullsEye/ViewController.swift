63252d283907b56e0a42e2cb4b6a386e49ebb99a
Dzmitry Kopats <dkopats@ebay.com>
2018-11-08 17:28:05 +0300

    Add BullEye project

---
+++ Ray Tutorials/BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Kopats, Dzmitry(AWF) on 11/8/18.
+//  Copyright © 2018 Kopats, Dzmitry(AWF). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        print("Hello")
+    }
+
+
+}
+
